660849f6929cf22a9190ec4da254fa2354004811
Gokula009 <106223088+Gokula009@users.noreply.github.com>
2024-04-10 12:31:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,21 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID","25169055")
+    API_HASH  = os.environ.get("API_HASH","d70362ef232cc5ec4cb0b8d1ba6cc60e")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","6345624057:AAHRTjVwaE1c1733O8rZ4SoEku8jD2XjXQg") 
 
     # database config
-    DB_NAME = os.environ.get("DB_NAME","madflixbotz")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_NAME = os.environ.get("DB_NAME","hipem80128")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://hipem80128:gokul2203@cluster0.asswrzi.mongodb.net/?retryWrites=true&w=majority
+")
  
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/4b306f4b15c23a8f22e58.jpg")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN','6629872386').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB","-1002034475672") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","-1002083865932"))
     
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
